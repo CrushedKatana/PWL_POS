74d67db11e6f58cc39c79725fa799b6bcef68f3d
CrushedKatana <charelsadewo123@gmail.com>
2025-03-04 10:08:49 +0700

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $data = [
+            'username' => 'customer-1',
+            'nama' => 'Pelanggan',
+            'password' => Hash::make('12345'),
+            'level_id' => 4
+        ];
+        UserModel::insert($data);
+
+
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+}
