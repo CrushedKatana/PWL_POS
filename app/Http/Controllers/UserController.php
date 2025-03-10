5b918912c9b928a360b072f472286ff57d7b1ee1
CrushedKatana <charelsadewo123@gmail.com>
2025-03-10 19:04:55 +0700

    com 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,7 +21,11 @@ class UserController extends Controller
         // UserModel::create($data);
 
         // $user = UserModel::all();
-        $user = UserModel::find(1);
+        // $user = UserModel::where('level_id', 1)->first();
+        // $user = UserModel::firstwhere('level_id', 1);
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
