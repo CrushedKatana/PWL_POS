65434495afa2d8380f0f684644131838bbd0db20
CrushedKatana <charelsadewo123@gmail.com>
2025-03-10 20:45:09 +0700

    com 2.7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -89,9 +89,12 @@ class UserController extends Controller
         // $user->wasChanged('nama'); // false 
         // dd($user->wasChanged(['nama', 'username'])); // true
 
-        $user = UserModel::all();
-         return view('user', ['data' => $user]);
-     }
+        // $user = UserModel::all();
+
+        $user = UserModel::with('level')->get();
+        // dd($user);
+        return view('user', ['data' => $user]);
+    }
      public function tambah()
      {
 
