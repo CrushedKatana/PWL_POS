c617566cb0dcb8733c6ce2e474a990cc9bc1082e
CrushedKatana <charelsadewo123@gmail.com>
2025-03-10 18:54:01 +0700

    commit 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,17 +10,18 @@ class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            // 'username' => 'manager_dua',
-            // 'nama' => 'Manager 2',
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password'=> Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     // 'username' => 'manager_dua',
+        //     // 'nama' => 'Manager 2',
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password'=> Hash::make('12345')
+        // ];
+        // UserModel::create($data);
 
-        $user = UserModel::all();
+        // $user = UserModel::all();
+        $user = UserModel::find(1);
         return view('user', ['data' => $user]);
     }
 }
