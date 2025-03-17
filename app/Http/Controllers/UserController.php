f70bdacd9e7395affe62f238a6111e0c586a1946
CrushedKatana <charelsadewo123@gmail.com>
2025-03-18 00:07:15 +0700

    commit prak 3 job 5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,142 +1,148 @@
 <?php
-
 namespace App\Http\Controllers;
-
+use App\Models\LevelModel;
 use App\Models\UserModel;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
 class UserController extends Controller
 {
+    // Menampilkan halaman awal user
     public function index()
     {
-        // $data = [
-        //     'level_id' => 2,
-        //     // 'username' => 'manager_dua',
-        //     // 'nama' => 'Manager 2',
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password'=> Hash::make('12345')
-        // ];
-        // UserModel::create($data);
+        $breadcrumb = (object) [
+            'title' => 'Daftar User',
+            'list' => ['Home', 'User']
+        ];
 
-        // $user = UserModel::all();
-        // $user = UserModel::where('level_id', 1)->first();
-        // $user = UserModel::firstwhere('level_id', 1);
-        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
-        //     abort(404);
-        // });
-        // $user = UserModel::where('username', 'manager9')->firstOrFail();
-        // $user = UserModel::where('level_id', 2)->count();
-        // dd($user);
-
-        // $user = UserModel::firstOrCreate(
-         //     [
-         //         'username' => 'manager22',
-         //         'nama' => 'Manager Dua Dua',
-         //         'password' => Hash::make('12345'),
-         //         'level_id' => 2
-         //     ],
-         // );
-
-        //  $user = UserModel::firstOrNew(
-        //     [
-        //         'username' => 'manager33',
-        //         'nama' => 'Manager Tiga Tiga',
-        //         'password' => Hash::make('12345'),
-        //         'level_id' => 2
-        //     ],
-        // );
-        // $user->save();
-
-        //  $user = UserModel::create([
-        //      'username' => 'manager55',
-        //      'nama' => 'Manager55',
-        //      'password' => Hash::make('12345'),
-        //      'level_id' => 2,
-        //  ]);
-        //  $user->username = 'manager56';
- 
-        //  $user->isDirty(); // true 
-        //  $user->isDirty('username'); // true 
-        //  $user->isDirty('nama'); // false 
-        //  $user->isDirty(['nama', 'username']); // true 
- 
-        //  $user->isClean(); // false 
-        //  $user->isClean('username'); // false 
-        //  $user->isClean('nama'); // true 
-        //  $user->isClean(['nama', 'username']); // false 
- 
-        //  $user->save();
- 
-        //  $user->isDirty(); // false 
-        //  $user->isClean(); // true
-        //  dd($user->isDirty());
-
-        // $user = UserModel::create([
-        //     'username' => 'manager11',
-        //     'nama' => 'Manager11',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 2,
-        // ]);
-        // $user->username = 'manager12';
-
-        // $user->save();
-        
-        // $user->wasChanged(); // true 
-        // $user->wasChanged('username'); // true 
-        // $user->wasChanged(['username', 'level_id']); // true 
-        // $user->wasChanged('nama'); // false 
-        // dd($user->wasChanged(['nama', 'username'])); // true
-
-        // $user = UserModel::all();
-
-        $user = UserModel::with('level')->get();
-        // dd($user);
-        return view('user', ['data' => $user]);
+        $page =(object)[
+        'title' => 'Daftar user yang terdaftar dalam sistem' 
+        ];
+        $activeMenu = 'user'; // set menu yang sedang aktif
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
     }
-     public function tambah()
-     {
-
-         return view('user_tambah');
-     }
-
-     public function tambah_simpan(Request $request)
+    
+    // Ambil data user dalam bentuk json untuk datatables
+    public function list(Request $request)
     {
+    $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
+    ->with('level');
+    return DataTables::of($users)
+    // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
+    ->addIndexColumn()
+    ->addColumn('aksi', function ($user) { // menambahkan kolom aksi
+    $btn = '<a href="'.url('/user/' . $user->user_id).'" class="btn btn-info btnsm">Detail</a> ';
+    $btn .= '<a href="'.url('/user/' . $user->user_id . '/edit').'" class="btn btnwarning btn-sm">Edit</a> ';
+    $btn .= '<form class="d-inline-block" method="POST" action="'.
+    url('/user/'.$user->user_id).'">'
+    . csrf_field() . method_field('DELETE') .
+    '<button type="submit" class="btn btn-danger btn-sm" onclick="return
+    confirm(\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';
+    return $btn;
+    })
+    ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html
+    ->make(true);
+    }
+    
+    // Menampilkan halaman form tambah user
+    public function create()
+    {
+    $breacrumb = (object) [
+        'title' => 'Tambah User',
+        'list' => ['Home', 'User', 'Tambah']
+    ];
+    $page = (object) [
+    'title' => 'Tambah user baru'
+    ];
+    $level = LevelModel::all(); // ambil data level untuk ditampilkan di form
+    $activeMenu = 'user'; // set menu yang sedang aktif
+    return view('user.create', ['breadcrumb' =>$breacrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
+    }
 
+    // Menyimpan data user baru
+    public function store (Request $request)
+    {
+        $request->validate([
+            'username' => 'required|string|min:3|unique:m_user,username',
+            'nama'     => 'required|string|max100',
+            'password' => 'required|min:5',
+            'level_id' => 'required|integer'
+        ]);
+    
         UserModel::create([
             'username' => $request->username,
-            'nama' => $request->nama,
-            'password' => Hash::make('$request->password'),
-            'level_id' => $request->level_id
+            'nama'     => $request->nama,
+            'password' => bcrypt($request->password),
+            'level id' => $request->level_id
         ]);
-        return redirect('/user');
+        return redirect('/user')->with('success', 'Data user berhasil disimpan');    
     }
 
-    public function ubah($id)
+    // Menampilkan detall user
+    public function show(string $id)
     {
+    $user = UserModel::with('level')->find($id);
+    $breadcrumb = (object) [
+    'title' => 'Detail User', 
+    'list' => ['Home', 'User', 'Detail']
+    ];
 
-        $user = UserModel::find($id);
-        return view('user_ubah', ['data' => $user]);
+    $page = (object) [
+    'title' => 'Detail user'
+    ];
+    $activeMenu = 'user'; 
+    return view('user.show', ['breadcrumb' => $breadcrumb, 'page' => $page, 'user' => $user, 'activeMenu' => $activeMenu]);
     }
 
-    public function ubah_simpan($id, Request $request)
+    // Menampilkan halaman form edit user
+    public function edit(string $id)
     {
+    $user = UserModel::find($id); 
+    $level = LevelModel::all();
+    $breadcrumb = (object) [
+    'title' => 'Edit User',
+    'list' => ['Home', 'User', 'Edit'] 
+    ];
 
-        $user = UserModel::find($id);
-        $user->username = $request->username;
-        $user->nama = $request->nama;
-        $user->password = Hash::make('$request->password');
-        $user->level_id = $request->level_id;
-        $user->save();
-        return redirect('/user');
+    $page = (object) [
+    'title' => 'Edit user'
+    ];
+
+    $activeMenu = 'user';
+    return view('user.edit', ['breadcrumb' => $breadcrumb, 'page' => $page, 'user' => $user, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
-    public function hapus($id)
-    {
+    public function update (Request $request, string $id){
+        $request->validate([
+            'username' => 'required|string|min:3|unique:m_user,username',
+            'nama'     => 'required|string|max100',
+            'password' => 'required|min:5',
+            'level_id' => 'required|integer'
+        ]);
+    
+        UserModel::create([
+            'username' => $request->username,
+            'nama'     => $request->nama,
+            'password' => bcrypt($request->password),
+            'level id' => $request->level_id
+        ]);
+    return redirect('/user')->with('success', 'Data user berhasil diubah');
+    }
 
-        $user = UserModel::find($id);
-        $user->delete();
-        return redirect('/user');
+    // Menghapus data user
+    public function destroy(string $id)
+    {
+    $check = UserModel::find($id);
+    if (!$check) {
+    return redirect('/user')->with('error', 'Data user tidak ditemukan');
+    }
+    
+    try{
+    UserModel::destroy($id);
+    return redirect('/user')->with('success', 'Data user berhasil dihapus');
+    }catch (\Illuminate\Database\QueryException $e) {
+    
+        return redirect('/user')->with('error', 'Data user gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
     }
 }
+};
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -22,51 +22,51 @@
     <p>Dashboard</p>
     </a>
     </li>
-    <li class="nav-header">User Data</li>
-    <li class="nav-item">
-    <a href="{{ url('/level') }}" class="nav-link {{ ($activeMenu == 'level')?
-    'active' : '' }} ">
-    <i class="nav-icon fas fa-layer-group"></i>
-    <p>Level User</p>
-    </a>
-    </li>
-    <li class="nav-item">
-    <a href="{{ url('/user') }}" class="nav-link {{ ($activeMenu == 'user')?
-    'active' : '' }}">
-    <i class="nav-icon far fa-user"></i>
-    <p>Data User</p>
-    </a>
-    </li>
-    <li class="nav-header">Item Data</li>
-    <li class="nav-item">
-    <a href="{{ url('/kategori') }}" class="nav-link {{ ($activeMenu ==
-    'kategori')? 'active' : '' }} ">
-    <i class="nav-icon far fa-bookmark"></i>
-    <p>Goods Category</p>
-    </a>
-    </li>
-    <li class="nav-item">
-    <a href="{{ url('/item') }}" class="nav-link {{ ($activeMenu == 'item')?
-    'active' : '' }} ">
-    <i class="nav-icon far fa-list-alt"></i>
-    <p>Goods Data</p>
-    </a>
-    </li>
-    <li class="nav-header">Transaction Data</li>
-    <li class="nav-item">
-    <a href="{{ url('/stock') }}" class="nav-link {{ ($activeMenu == 'stock')?
-    'active' : '' }} ">
-    <i class="nav-icon fas fa-cubes"></i>
-    <p>Stock of Goods</p>
-    </a>
-    </li>
-    <li class="nav-item">
-    <a href="{{ url('/item') }}" class="nav-link {{ ($activeMenu == 'sales')?
-    'active' : '' }} ">
-    <i class="nav-icon fas fa-cash-register"></i>
-    <p>Sales Transactions</p>
-    </a>
-    </li>
-    </ul>
-    </nav>
-    </div>
+    <li class="nav-header">Data Pengguna</li>
+<li class="nav-item">
+<a href="{{ url('/level') }}" class="nav-link {{ ($activeMenu == 'level')?
+'active' : '' }} ">
+<i class="nav-icon fas fa-layer-group"></i>
+<p>Level User</p>
+</a>
+</li>
+<li class="nav-item">
+<a href="{{ url('/user') }}" class="nav-link {{ ($activeMenu == 'user')?
+'active' : '' }}">
+<i class="nav-icon far fa-user"></i>
+<p>Data User</p>
+</a>
+</li>
+<li class="nav-header">Data Barang</li>
+<li class="nav-item">
+<a href="{{ url('/kategori') }}" class="nav-link {{ ($activeMenu ==
+'kategori')? 'active' : '' }} ">
+<i class="nav-icon far fa-bookmark"></i>
+<p>Kategori Barang</p>
+</a>
+</li>
+<li class="nav-item">
+<a href="{{ url('/barang') }}" class="nav-link {{ ($activeMenu ==
+'barang')? 'active' : '' }} ">
+<i class="nav-icon far fa-list-alt"></i>
+<p>Data Barang</p>
+</a>
+</li>
+<li class="nav-header">Data Transaksi</li>
+<li class="nav-item">
+<a href="{{ url('/stok') }}" class="nav-link {{ ($activeMenu == 'stok')?
+'active' : '' }} ">
+<i class="nav-icon fas fa-cubes"></i>
+<p>Stok Barang</p>
+</a>
+</li>
+<li class="nav-item">
+<a href="{{ url('/barang') }}" class="nav-link {{ ($activeMenu ==
+'penjualan')? 'active' : '' }} ">
+<i class="nav-icon fas fa-cash-register"></i>
+<p>Transaksi Penjualan</p>
+</a>
+</li>
+</ul>
+</nav>
+</div>
